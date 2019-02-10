0afe677a7fd07cf69db6493856ba4b1a388c9d17
Jonathan Kalish <CiniMinis@users.noreply.github.com>
2019-02-10 19:39:21 +0200

    Documented Motion Profile
    Documented motion profile code and arranged it to be readable

--- Motion Profiling/Experiments/Mathematic-Trajectory/graph.py
+++ Motion Profiling/Experiments/Mathematic-Trajectory/graph.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import math
 
-X = -0.4
-Y = 1.8
-L = 0.6
-max_accel = 2
-max_velo = 3
-point_num = 100
+X = -0.4                # X coord of target
+Y = 1.8                 # Y coord of target
+L = 0.6                 # Robot width
+max_accel = 2           # maximal acceleration permitted for the robot
+max_velo = 3            # maximal velocity permitted for the robot
+point_num = 100         # the number of points generated along the path
+angle = math.pi / 3     # the angle the robot finishes at
 
-angle = math.pi / 3
+# heuristically calculated weights in the path
 w1 = Y
 w2 = math.sqrt(X ** 2 + Y ** 2)
+# the coefficients of the path generated
 Ax = (np.cos(angle) * w2 - 2 * X)
 Ay = (w1 + np.sin(angle) * w2 - 2 * Y)
 Bx = 3 * X - np.cos(angle) * w2
@@ -23,25 +25,44 @@ Cy = w1
 axes = plt.axes(projection='3d')
 
 axes.set_aspect('equal')
-# Data for a three-dimensional line
 z = np.linspace(0, 1, point_num)
+
+# path for the robots center
 xline = Ax * z ** 3 + Bx * z ** 2
 yline = Ay * z ** 3 + By * z ** 2 + Cy * z
+
+# derivatives of the path to calculate heading
 RePrime = 3 * Ax * z ** 2 + 2 * Bx * z
 ImPrime = 3 * Ay * z ** 2 + 2 * By * z + Cy
+
+# difference in both axes for the right and left motors
 dy = L/2 * np.sin(math.pi - np.arctan(-1 * RePrime / ImPrime))
 dx = L/2 * np.cos(math.pi - np.arctan(-1 * RePrime / ImPrime))
 
+# left and right wheel paths
+l_wheel_x = xline + dx
+l_wheel_y = yline - dy
+r_wheel_x = xline - dx
+r_wheel_y = yline + dy
+
+# numpy arrays for the motion profile (location, velocity and time)
 l_vel = np.zeros(point_num)
 r_vel = np.zeros(point_num)
 l_time = np.zeros(point_num)
 r_time = np.zeros(point_num)
+l_loc = np.zeros(point_num)
+r_loc = np.zeros(point_num)
 
+# calculate velocities going forward
 for i in range(1, point_num-1):
-    l_dist = np.sqrt((xline[i] + dx[i] - xline[i - 1] - dx[i - 1]) ** 2 + (yline[i] - dy[i] - yline[i - 1] + dy[i - 1]) ** 2)
-    r_dist = np.sqrt((xline[i] - dx[i] - xline[i - 1] + dx[i - 1]) ** 2 + (yline[i] + dy[i] - yline[i - 1] - dy[i - 1]) ** 2)
+    l_dist = np.sqrt((l_wheel_x[i] - l_wheel_x[i - 1]) ** 2 +
+                     (l_wheel_y[i] - l_wheel_y[i - 1]) ** 2)
+    r_dist = np.sqrt((r_wheel_x[i] - r_wheel_x[i - 1]) ** 2 +
+                     (r_wheel_y[i] - r_wheel_y[i - 1]) ** 2)
+    # calculates right velocity based on the left maximum
     l_vel[i] = np.sqrt(l_vel[i-1]**2 + 2 * max_accel * l_dist)
     r_vel[i] = (l_vel[i-1] + l_vel[i]) * (r_dist / l_dist) - r_vel[i-1]
+    # enforcing max velocity limitation
     if l_vel[i] > max_velo:
         l_vel[i] = max_velo
         r_vel[i] = (l_vel[i-1] + l_vel[i]) * (r_dist / l_dist) - r_vel[i-1]
@@ -49,11 +70,16 @@ for i in range(1, point_num-1):
         r_vel[i] = max_velo
         l_vel[i] = (r_vel[i-1] + r_vel[i]) * (l_dist / r_dist) - l_vel[i-1]
 
+# calculates deacceleration limits
 for i in range(point_num-1, 1, -1):
-    l_dist = np.sqrt((xline[i] + dx[i] - xline[i - 1] - dx[i - 1]) ** 2 + (yline[i] - dy[i] - yline[i - 1] + dy[i - 1]) ** 2)
-    r_dist = np.sqrt((xline[i] - dx[i] - xline[i - 1] + dx[i - 1]) ** 2 + (yline[i] + dy[i] - yline[i - 1] - dy[i - 1]) ** 2)
+    l_dist = np.sqrt((l_wheel_x[i] - l_wheel_x[i - 1]) ** 2 +
+                     (l_wheel_y[i] - l_wheel_y[i - 1]) ** 2)
+    r_dist = np.sqrt((r_wheel_x[i] - r_wheel_x[i - 1]) ** 2 +
+                     (r_wheel_y[i] - r_wheel_y[i - 1]) ** 2)
+    # calculates the max velocity needed for slowing down
     l_vel_max = np.sqrt(l_vel[i]**2 + 2 * max_accel * l_dist)
     r_vel_max = np.sqrt(r_vel[i] ** 2 + 2 * max_accel * r_dist)
+    # recalculates velocity to match deacceleration limits
     if l_vel[i-1] > l_vel_max:
         l_vel[i-1] = l_vel_max
         r_vel[i-1] = (l_vel[i-1] + l_vel[i]) * (r_dist / l_dist) - r_vel[i]
@@ -61,15 +87,22 @@ for i in range(point_num-1, 1, -1):
         r_vel[i-1] = r_vel_max
         l_vel[i-1] = (r_vel[i-1] + r_vel[i]) * (l_dist / r_dist) - l_vel[i]
 
+# calculates final time and distance per point
 for i in range(1, point_num):
-    l_dist = np.sqrt((xline[i] + dx[i] - xline[i - 1] - dx[i - 1]) ** 2 + (yline[i] - dy[i] - yline[i - 1] + dy[i - 1]) ** 2)
-    r_dist = np.sqrt((xline[i] - dx[i] - xline[i - 1] + dx[i - 1]) ** 2 + (yline[i] + dy[i] - yline[i - 1] - dy[i - 1]) ** 2)
+    l_dist = np.sqrt((l_wheel_x[i] - l_wheel_x[i - 1]) ** 2 +
+                     (l_wheel_y[i] - l_wheel_y[i - 1]) ** 2)
+    r_dist = np.sqrt((r_wheel_x[i] - r_wheel_x[i - 1]) ** 2 +
+                     (r_wheel_y[i] - r_wheel_y[i - 1]) ** 2)
+    # calculates left and right times (should be identical)
     r_time[i] = r_time[i-1] + 2 * r_dist / (r_vel[i] + r_vel[i-1])
     l_time[i] = l_time[i-1] + 2 * l_dist / (l_vel[i] + l_vel[i-1])
-    print r_time[i] - l_time[i]
+    # calculates total distance traveled per side
+    l_loc[i] = l_loc[i-1] + l_dist
+    r_loc[i] = r_loc[i-1] + r_dist
 
+# draws and displays generated trajectories
 axes.scatter(xline, yline, z)
-axes.scatter(xline + dx, yline - dy, z, c=l_vel, cmap=plt.spring())
-axes.scatter(xline - dx, yline + dy, z, c=r_vel, cmap=plt.spring())
+axes.scatter(l_wheel_x, l_wheel_y, z, c=l_vel, cmap=plt.spring())
+axes.scatter(r_wheel_x, r_wheel_y, z, c=r_vel, cmap=plt.spring())
 
 plt.show()
