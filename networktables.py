822b932cfb09a7dc6d89c43febc22e5b6e520874
Yotam Cohen <isyotamc@gmail.com>
2019-02-20 05:45:58 +0200

    PEP82SDFKLGSDFKLGJSKDFG2SDFKLGSDFKLGJSKDFG!

--- networktables.py
+++ networktables.py
@@ -1,5 +1,5 @@
 import threading
-from networktables import NetworkTables 
+from networktables import NetworkTables
 
 SKYNET_IP = '10.44.16.2'
 VISION_TABLE_NAME = "vision"
@@ -17,81 +17,81 @@ def init_and_wait():
     """A blocking function that will init and wait"""
     global VISION_TABLE
     VISION_TABLE = NetworkTables.getTable(VISION_TABLE_NAME)
-    cond = threading.Condition()	
+    cond = threading.Condition()
     notified = [False]
 
     def connectionListener(connected, info):
-	print(info, '; Connected=%s' % connected)
-	with cond:
-	    notified[0] = True
-	    cond.notify()
+        print("Connected: {} \n info : {}".format(connected, info))
+        with cond:
+            notified[0] = True
+            cond.notify()
 
     NetworkTables.initialize(server=SKYNET_IP)
     NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
     with cond:
-	if not notified[0]:
-	    cond.wait()
-    
+        if not notified[0]:
+            cond.wait()
+
 
 def get_bool(key: str, default: bool) -> bool:
     if VISION_TABLE is None:
-	raise Exception("vision_table Wasn't initialized, please use init")
+        raise Exception("vision_table Wasn't initialized, please use init")
     return VISION_TABLE.getBoolean(key, default)
+
 
 def set_bool(key: str, value: bool):
     if VISION_TABLE is None:
-	raise Exception("vision_table Wasn't initialized, please use init")
+        raise Exception("vision_table Wasn't initialized, please use init")
     VISION_TABLE.putBoolean(key, value)
-    
+
+
 def profile_request_listener(table, key, value, isNew):
     """A listener for waiting for the profile bool to be true"""
     if key != VISION_START_KEY:
-	return
-	
+        return
+
     with profile_listener_cond_var:
-	global profile_listener_state
-	profile_listener_state = value
-	profile_listener_cond_var.notify()
-        
+        global profile_listener_state
+        profile_listener_state = value
+        profile_listener_cond_var.notify()
+
 
 def wait_for_profile_request():
     """Wait for a motion profile request from the main robot"""
     # I know that this is ugly, but I cba to add a decorator
     if VISION_TABLE is None:
-	raise Exception("vision_table Wasn't initialized, please use init")
-    
+        raise Exception("vision_table Wasn't initialized, please use init")
+
     global profile_listener_cond_var
     global profile_listener_state
-    
+
     profile_listener_cond_var = threading.Condition()
     profile_entry = NetworkTables.getEntry(VISION_START_KEY)
     profile_entry.addEntryListener(profile_request_listener)
-    
+
     while profile_listener_cond_var:
-	if not profile_listener_state:
-	    profile_listener_cond_var.wait()
-    
+        if not profile_listener_state:
+            profile_listener_cond_var.wait()
+
     PROFILE_LISTENER_STATE = False
-    
-    
+
+
 def send_motion_profiles(leftProfile, rightProfile):
     """Sends the motion profiles to the network table and turns off the required flag
     :param leftProfile, rightProfile: motion profiling profiles. Arrays sized 100x3"""
     leftEntries = [NetworkTables.getEntry(key) for key in LEFT_PROFILE_KEYS]
     rightEntries = [NetworkTables.getEntry(key) for key in RIGHT_PROFILE_KEYS]
-    
+
     for i in range(3):
-	leftEntries[i].putDoubleArray(leftProfile[i])
-	rightEntries[i].putDoubleArray(rightProfile[i])
+        leftEntries[i].putDoubleArray(leftProfile[i])
+        rightEntries[i].putDoubleArray(rightProfile[i])
 
     set_bool(VISION_START_KEY, False)
+
 
 def main():
     pass
 
 if __name__ == "__main__":
     main()
-	
-	
-
