8aa4498c261579dbfde943bc04e2bf31f25798a0
Nir <nir61202@gmail.com>
2019-02-21 13:14:47 +0200

    add a decorator instead of ifs every time

--- network_table_profile_api.py
+++ network_table_profile_api.py
@@ -12,6 +12,14 @@ VISION_TABLE = None
 profile_listener_cond_var = None
 profile_listener_state = False
 
+def require_vision(func):
+    def _require_vision(*args, **kwargs):
+        if VISION_TABLE is None:
+            print("I'm not connected to the vision table. Please initialize it with init_and_wait().")
+            return
+        return func(*args, **kwargs)
+    return _require_vision
+
 
 def init_and_wait():
     """A blocking function that will init and wait"""
@@ -33,18 +41,12 @@ def init_and_wait():
         if not notified[0]:
             cond.wait()
 
-
+@require_vision
 def get_bool(key: str, default: bool) -> bool:
-    if VISION_TABLE is None:
-        print("vision_table wasn't initialized, please use init")
-        return
     return VISION_TABLE.getBoolean(key, default)
 
-
+@require_vision
 def set_bool(key: str, value: bool):
-    if VISION_TABLE is None:
-        print("vision_table wasn't initialized, please use init")
-        return
     VISION_TABLE.putBoolean(key, value)
 
 
@@ -58,13 +60,9 @@ def profile_request_listener(table, key, value, isNew):
         profile_listener_state = value
         profile_listener_cond_var.notify()
 
-
+@require_vision
 def wait_for_profile_request():
     """Wait for a motion profile request from the main robot"""
-    # I know that this is ugly, but I cba to add a decorator
-    if VISION_TABLE is None:
-        raise Exception("vision_table wasn't initialized, please use init")
-
     global profile_listener_cond_var
     global profile_listener_state
 
@@ -76,9 +74,9 @@ def wait_for_profile_request():
         if not profile_listener_state:
             profile_listener_cond_var.wait()
 
-    PROFILE_LISTENER_STATE = False
+    profile_listener_state = False
 
-
+@require_vision
 def send_motion_profiles(leftProfile, rightProfile):
     """Sends the motion profiles to the network table and turns off the required flag
     :param leftProfile, rightProfile: motion profiling profiles. Arrays sized 100x3"""
@@ -91,9 +89,5 @@ def send_motion_profiles(leftProfile, rightProfile):
 
     set_bool(VISION_START_KEY, False)
 
-
-def main():
-    pass
-
 if __name__ == "__main__":
-    main()
+    print("This is a library, so you can't run it as main.")
